76bfa50974463d6b607743824c69b5711e1825d6
BDH <baekdh06@gmail.com>
2026-01-27 12:53:22 +0900

    For문 배움

---
+++ Project10/Project10.cpp
@@ -0,0 +1,50 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main()
+{
+    //사칙 연산 수행하는 계산기 프로그램 만들기, 두개의 정수와 연산자를 입력받아서 해당 연산 수행하기, x누르면 종료
+
+    int n1, n2;
+    char ch1,tmp;
+    do
+    {
+        printf("첫번째 정수를 입력하세요: ");
+        scanf("%d", &n1);
+        printf("다음 연산자 중 하나를 입력하세요(+,-,*,/), x 입력시 종료: ");
+        scanf("%c", &tmp);
+        scanf("%c", &ch1);
+        scanf("%c", &tmp);
+        printf("두번째 정수를 입력하세요: ");
+        scanf("%d", &n2);
+
+        switch (ch1)
+        {
+        case '+':
+            printf("\n %d+%d=%d\n", n1, n2, n1 + n2);
+            break;
+        case '-':
+            printf("\n%d-%d=%d\n", n1, n2, n1 - n2);
+            break;
+        case '*':
+            printf("\n%d*%d=%d\n", n1, n2, n1 * n2);
+            break;
+        case '/':
+            printf("\n%d/%d=%d\n", n1, n2, n1 / n2);
+            break;
+        case 'x':
+            break;
+        default:
+            printf("연산자를 잘못 입력했습니다. 다시 입력해주세요.\n");
+        }
+        printf("\n");
+        } while (ch1 != 'x');
+
+        printf("프로그램이 종료되었습니다.");
+
+
+        return 0;
+    }
+
+
---
+++ Project11/Project11.cpp
@@ -0,0 +1,61 @@
+﻿#define _CRT_SECURE_NO_WARNINGS
+
+#include <stdio.h>
+
+int main()
+{
+	/* for 반복문
+	* 
+	* for(초기식 ; 조건식 ; 증감식 )
+	* {
+	*	//반복실행할 명령문
+	* 
+	* }
+	
+	
+	
+	*/
+	// int i = 0;
+/*	while (i < 10)
+	{
+		printf("i = %d\n", i);
+		i++;
+	}
+	*/
+	//for (int i = 0; i < 10; i++)	//초기식 ==> 조건식 ==> 명령문 ==>증감식 ==>조건식
+	//{
+	//	printf("i= %d\n", i);
+	//}
+	//for(반복진입하면서 처음에 무조건 한번만 실행할 명령문(들); 처음부터 매 반복시 실행될 명령문, 이 결과가 0이면 반복종료; 1회 반복이후부터 매 반복시 실행될 명령문)
+
+	for (int i = 0; i < 5; printf("%d\n", i++))
+	{
+
+	}
+
+	// 2 4 6 8 10
+	for (int j = 0; j < 10; printf(" % d\t", j += 2))
+	{
+
+	}
+
+	for (char ch = 'a'; ch <= 'z'; printf("%c", ch++))
+	{
+
+	}
+
+	printf("\n");
+
+	for (char ch1 = 'z'; ch1 >= 'a'; ch1--)
+	{
+		printf("%c", ch1-32);
+	}
+	printf("\n");
+
+	for (int i = 0; ; i++)
+	{
+		if (i >= 10) break; //반복문 즉시 탈출
+		printf("%d", i);
+	}
+	return 0;
+}
